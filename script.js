53bbbf239925c20e3e2a1419c846f473070efb92
ilkiinz <aqayev2205@gmail.com>
2023-04-16 17:54:31 +0400

    js change

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-// tap 1//
+// tap 1
 let msSpeed;
  msSpeed = 20.8333;
  console.log(msSpeed);
 
-// tap 2//
+// tap 2
 let num = 57;
 console.log(num%3);
 console.log(num ** 5)
 
-// tap 3//
+// tap 3
 const firstName = 'Alan';
 const lastName = 'Turing';
 const FullName = firstName + ' ' + lastName;
 console.log(FullName, 'Tanışlığımıza çox şadam!')
 
-// tap 4//
+// tap 4
 let firstNam = 'Alan';
 let lastNam = 'Turing';
 let greet = `Sizi bir daha görməyimizə şadıq ${firstNam} ${lastNam}`;
 console.log(greet)
 
-// tap 5 //
+// tap 5 
 
 let fullName = 'Alan Turing' ;
 console.log(fullName.length);
 
-// tap 6//
+// tap 6
 const ad = 'Alan';
 const soyad= 'Turing';
 const tamAd = firstName + ' ' + lastName;
 console.log(ad.charAt (0))
 console.log(soyad.charAt(0))
+
+// tap 7
+let Full = prompt("zehmet olmasa ad ve soyad daxil edin.");
+console.log(Full);
