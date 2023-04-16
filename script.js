5b5807d4fd42b2b3aa3fa662bbc1f74fe757d5e5
ilkiinz <aqayev2205@gmail.com>
2023-04-16 16:52:36 +0400

    js change

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// tap 1//
 let msSpeed;
  msSpeed = 20.8333;
  console.log(msSpeed);
+
+// tap 2//
+let num = 57;
+console.log(num%3);
+console.log(num ** 5)
+
+// tap 3//
+const firstName = 'Alan';
+const lastName = 'Turing';
+const FullName = firstName + '' + lastName;
+console.log(FullName Tanışlığımıza çox şadam! )
